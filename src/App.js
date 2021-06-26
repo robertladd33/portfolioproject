30fe3850159c1de111675caf1bb8ebd3d52dbcc7
Robert <robertladd1027@gmail.com>
2021-06-25 22:53:17 -0700

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from "react-particles-js";
 import Navbar from './components/Navbar';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Particles className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 1000
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 8,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
